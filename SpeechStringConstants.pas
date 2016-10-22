9180bc8dc22c70f53368e6504fc37ada029da7db
coder-pig <35927925@qq.com>
2016-10-23 01:23:23 +0800

    SpeechStringConstants.pas

---
+++ SpeechStringConstants.pas
@@ -0,0 +1,45 @@
+unit SpeechStringConstants;
+
+interface
+
+    const SpeechAddRemoveWord = 'AddRemoveWord';
+    const SpeechAudioFormatGUIDText = '{7CEEF9F9-3D13-11d2-9EE7-00C04F797396}';
+    const SpeechAudioFormatGUIDWave = '{C31ADBAE-527F-4ff5-A230-F62BB61FF70C}';
+    const SpeechAudioProperties = 'AudioProperties';
+    const SpeechAudioVolume = 'AudioVolume';
+    const SpeechCategoryAppLexicons = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\AppLexicons';
+    const SpeechCategoryAudioIn = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\AudioInput';
+    const SpeechCategoryAudioOut = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\AudioOutput';
+    const SpeechCategoryPhoneConverters = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\PhoneConverters';
+    const SpeechCategoryRecognizers = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Recognizers';
+    const SpeechCategoryRecoProfiles = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Speech\RecoProfiles';
+    const SpeechCategoryVoices = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices';
+    const SpeechDictationTopicSpelling = 'Spelling';
+    const SpeechEngineProperties = 'EngineProperties';
+    const SpeechGrammarTagDictation = '*';
+    const SpeechGrammarTagUnlimitedDictation = '*+';
+    const SpeechGrammarTagWildcard = '...';
+    const SpeechMicTraining = 'MicTraining';
+    const SpeechPropertyAdaptationOn = 'AdaptationOn';
+    const SpeechPropertyComplexResponseSpeed = 'ComplexResponseSpeed';
+    const SpeechPropertyHighConfidenceThreshold = 'HighConfidenceThreshold';
+    const SpeechPropertyLowConfidenceThreshold = 'LowConfidenceThreshold';
+    const SpeechPropertyNormalConfidenceThreshold = 'NormalConfidenceThreshold';
+    const SpeechPropertyResourceUsage = 'ResourceUsage';
+    const SpeechPropertyResponseSpeed = 'ResponseSpeed';
+    const SpeechRecoProfileProperties = 'RecoProfileProperties';
+    const SpeechRegistryLocalMachineRoot = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech';
+    const SpeechRegistryUserRoot = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Speech';
+    const SpeechTokenIdUserLexicon = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Speech\CurrentUserLexicon';
+    const SpeechTokenKeyAttributes = 'Attributes';
+    const SpeechTokenKeyFiles = 'Files';
+    const SpeechTokenKeyUI = 'UI';
+    const SpeechTokenValueCLSID = 'CLSID';
+    const SpeechUserTraining = 'UserTraining';
+    const SpeechVoiceCategoryTTSRate = 'DefaultTTSRate';
+    const SpeechVoiceSkipTypeSentence = 'Sentence';
+
+implementation
+
+
+end.
